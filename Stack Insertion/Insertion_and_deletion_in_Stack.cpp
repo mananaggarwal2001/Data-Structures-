7b8ea896eca4522934ccf618794d17362303274a
Manan Aggarwal <mananaggarwal2001@gmail.com>
2021-10-04 23:54:56 +0530

    Stack Modification

--- Stack Insertion/Insertion_and_deletion_in_Stack.cpp
+++ Stack Insertion/Insertion_and_deletion_in_Stack.cpp
@@ -3,7 +3,7 @@ using namespace std;
 struct Stack
 {
     int size;
-    int top = -1;
+    int top;
     int *arr;
 };
 
@@ -20,7 +20,26 @@ public:
     void push();
     void pop();
     void display();
+    void peek();
 };
+
+void Stack_Class ::peek()  // peek operation in the stack
+{
+    int position;
+    cout<<"Start Entering the position from the 1 not from the 0!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"<<endl;
+    cout << "Enter the position of the element you want in stack from the top: ";
+    cin >> position;
+    if (position > object->top || position == -1)
+    {
+        cout << "Invalid position entered try again";
+    }
+    else
+    {
+        int var= object->arr[object->top-position +1];
+        cout<<"The value of the stack at the "<< position <<" position is: "<<var;
+    }
+    cout<<endl;
+}
 
 bool Stack_Class ::isEmpty()
 {
@@ -49,8 +68,8 @@ bool Stack_Class ::isFull()
 void Stack_Class ::creation()
 {
     int s;
-    object->top = 0;
     char ch = 'y';
+    object->top = -1;
     cout << "Enter the size of the stack: ";
     cin >> s;
     object->size = s;
@@ -59,30 +78,57 @@ void Stack_Class ::creation()
     {
         if (isFull())
         {
-            cout<<"The Stack is Full";
+            cout << "The Stack is Full" << endl;
             break;
         }
         else
         {
 
-            cout << "Enter the element " << object->top<<" : ";
+            object->top++;
+            cout << "Enter the element " << object->top << " : ";
             cin >> object->arr[object->top];
             cout << endl;
             cout << "Do you want to enter more: ";
             cin >> ch;
-            object->top++;
         }
     }
 }
 
-
 void Stack_Class::display()
 {
-    for (int i = 0; i < object->top; i++)
+    for (int i = 0; i <= object->top; i++)
     {
         cout << object->arr[i] << " ";
     }
     cout << endl;
+}
+
+void Stack_Class ::push()
+{
+    if (isFull())
+    {
+        cout << "Stack OverFlow!!!!!!!!!!!" << endl;
+    }
+    else
+    {
+        int element;
+        cout << "Enter the element which you want to insert in the Stack: ";
+        cin >> element;
+        object->arr[object->top] = element;
+        object->top++;
+    }
+}
+
+void Stack_Class ::pop()
+{
+    if (isEmpty())
+    {
+        cout << "Stack UnderFlow!!!!!!!!!!!!" << endl;
+    }
+    else
+    {
+        object->top--;
+    }
 }
 int main()
 {
@@ -91,10 +137,11 @@ int main()
     do
     {
         cout << "1. Enter the array" << endl;
-        cout << "2. Display the array" << endl;
-        cout << "3. Push the element into the array" << endl;
-        cout << "4. pop the value in the array" << endl;
-        cout << "5. Exit" << endl;
+        cout << "2. Display the stack" << endl;
+        cout << "3. Push the element into the stack" << endl;
+        cout << "4. pop the value in the stack" << endl;
+        cout << "5. Showing the particular value of the stack" << endl;
+        cout << "6. Exit" << endl;
         cout << endl;
         cout << "Enter your choice: ";
         cin >> choice;
@@ -110,13 +157,33 @@ int main()
             operations.display();
             break;
         }
+        case 3:
+        {
+            operations.push();
+            break;
+        }
+        case 4:
+        {
+            operations.pop();
+            break;
+        }
+        case 5:
+        {
+            operations.peek();
+            break;
+        }
+        case 6:
+        {
+
+            break;
+        }
 
         default:
         {
-            cout << "WRONG CHOICE !!!!!!!!!";
+            cout << "WRONG CHOICE !!!!!!!!!" << endl;
             break;
         }
         }
-    } while (choice != 5);
+    } while (choice != 6);
     return 0;
 }
