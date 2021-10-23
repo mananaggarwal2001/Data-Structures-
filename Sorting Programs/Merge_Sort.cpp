a8be10aff4c1e2ce52467a27143188c57272622a
Manan Aggarwal <mananaggarwal2001@gmail.com>
2021-10-23 20:14:17 +0530

    Merge Sort Algorithm

---
+++ Sorting Programs/Merge_Sort.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+void PrintArray(int array[], int size) // function used for displaying the array.
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << array[i];
+        if (i == size - 1)
+        {
+            cout << " ";
+        }
+        else
+        {
+            cout << ",";
+        }
+    }
+    cout << endl;
+}
+void Merge_Sort_procedure(int arr[], int low, int mid, int high)
+{
+    int T_Arr[high + 1], i, j, k;
+    i = low;
+    j = mid + 1;
+    int count=low;
+    while (i <= mid && j <= high)
+    {
+        if (arr[i] <= arr[j])
+        {
+            T_Arr[count] = arr[i];
+            i++;
+            count++;
+        }
+        else if(arr[i] >= arr[j])
+        {
+            T_Arr[count] = arr[j];
+            j++;
+            count++;
+        }
+    }
+
+    while (i <= mid)
+    {
+        T_Arr[count] = arr[i];
+        i++;
+        count++;
+    }
+
+    while (j <= high)
+    {
+        T_Arr[count] = arr[j];
+        j++;
+        count++;
+    }
+
+    for (int i = low; i < (high + 1); i++)  // running i from the low will give the answer.
+    {
+        arr[i] = T_Arr[i]; // while transferring the array the changes would be occur on the same position in the array.
+    }
+}
+
+void Merge_Sort(int arr[], int low, int high)
+{
+    int mid;
+    if (low<high)
+    {
+        mid=(low+high)/2; // for doing the partitioning in the array.
+        Merge_Sort(arr,low,mid); // for the Sorting of the  left half.
+        Merge_Sort(arr,mid+1,high); // for the sorting of the right half.
+        Merge_Sort_procedure(arr,low,mid,high); // for the sorting of the array.
+    }
+
+}
+
+int main()
+{
+    int size;
+    cout << "Enter the size of the Array: ";
+    cin >> size;
+    int arr[size];
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Enter the element " << i + 1 << " in the Array : ";
+        cin >> arr[i];
+    }
+    Merge_Sort(arr, 0, size-1);
+    cout << "Sorted Array is: " << endl;
+    PrintArray(arr,size);
+
+    return 0;
+}
---
+++ Sorting Programs/Merging_Sort.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+void PrintArray(int array[], int size) // function used for displaying the array.
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << array[i];
+        if (i == size - 1)
+        {
+            cout << " ";
+        }
+        else
+        {
+            cout << ",";
+        }
+    }
+    cout << endl;
+}
+
+void Merging(int Sub_Array1[], int Sub_Array2[], int CombinedArray[], int sub_size1, int sub_size2, int combined_size3)
+{
+    int count_1, i, j, k;
+    count_1 = i = j = k = 0;
+    while (i < sub_size1 && j < sub_size2)
+    {
+        if (Sub_Array1[i] <= Sub_Array2[j])
+        {
+            CombinedArray[count_1] = Sub_Array1[i];
+            count_1++;
+            i++;
+        }
+        else if (Sub_Array1[i] >= Sub_Array2[j])
+        {
+            CombinedArray[count_1] = Sub_Array2[j];
+            count_1++;
+            j++;
+        }
+    }
+
+    while (i < sub_size1)
+    {
+        CombinedArray[count_1] = Sub_Array1[i];
+        count_1++;
+        i++;
+    }
+    while (j < sub_size2)
+    {
+        CombinedArray[count_1] = Sub_Array2[j];
+        count_1++;
+        j++;
+    }
+}
+int main()
+{
+    int size1, size2, combinedSize;
+    cout << "Enter the size of the First Array: ";
+    cin >> size1;
+    cout << "Enter the size of the Second Array: ";
+    cin >> size2;
+    combinedSize = size1 + size2;
+    int arr1[size1], arr2[size2], combined_Arr[combinedSize];
+    cout << "Enter the elements for the sub array 1" << endl;
+    for (int i = 0; i < size1; i++)
+    {
+        cout << "Enter the element " << i + 1 << " : ";
+        cin >> arr1[i];
+    }
+    cout << "Enter the elements for the sub array 2" << endl;
+    for (int i = 0; i < size2; i++)
+    {
+        cout << "Enter the element " << i + 1 << " : ";
+        cin >> arr2[i];
+    }
+    Merging(arr1, arr2, combined_Arr, size1, size2, combinedSize);
+    cout<<"The Sorted Array is: "<<endl;
+    PrintArray(combined_Arr, combinedSize);
+
+    return 0;
+}
