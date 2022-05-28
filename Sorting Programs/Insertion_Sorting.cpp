bc3f7bd1f6563056a37a3792e5f8803c6b05651d
Manan Aggarwal <mananaggarwal2001@gmail.com>
2022-05-28 18:05:53 +0530

    Insertion Sort Modification

--- Sorting Programs/Insertion_Sorting.cpp
+++ Sorting Programs/Insertion_Sorting.cpp
@@ -6,38 +6,39 @@ void insertion_Sorting(int arr[], int size)
     int element;
     int position;
     int j;
-for (int i = 1; i < size; i++)
-{
-    element = arr[i];
-    position= i-1;
-    for (j=position; j >-1 ; j--)
+    for (int i = 1; i < size; i++)
     {
-        if(arr[j]<element){  // insertion sorting decending condition.
-            arr[j+1]=arr[j];
-            arr[j]=element;
+        element = arr[i];
+        position = i - 1;
+        for (j = position; j > -1; j--)
+        {
+            if (arr[j] < element)
+            { // insertion sorting decending condition.
+                arr[j + 1] = arr[j];
+                arr[j] = element;
+            }
+            // if(arr[j]>element){  // insertion sorting ascending condition.
+            //     arr[j+1]=arr[j];
+            //     arr[j]=element;
+            // }
         }
-        // if(arr[j]<element){  // insertion sorting ascending condition.
-        //     arr[j+1]=arr[j];
-        //     arr[j]=element;
-        // }
     }
-
-}
     for (int i = 0; i < size; i++)
     {
         cout << arr[i];
-        if (i==size-1)
+        if (i == size - 1)
         {
-            cout<<"";
-        }else{
-            cout<<",";
+            cout << "";
         }
-
+        else
+        {
+            cout << ",";
+        }
     }
 }
 int main()
 {
-    int Array[] = {56,32,100,5,1,0,10,15};
+    int Array[] = {56, 32, 100, 5, 1, 0, 10, 15};
     int size = 8;
     insertion_Sorting(Array, size);
     return 0;
