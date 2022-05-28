d90f53420837b19686be4d1300ad38620026de16
Manan Aggarwal <mananaggarwal2001@gmail.com>
2022-05-28 13:05:41 +0530

    Bubble Sort modified

--- Sorting Programs/Bubble_sort.cpp
+++ Sorting Programs/Bubble_sort.cpp
@@ -1,14 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-void bubbleSortProcessShow(int arr[], int size)
+class Bubble_class // Template for the Bubble Sort Algorithm checking.
 {
-    
+
+public:
+    void bubbleSortProcessShow(int arr[], int size);
+    void bubbleSort(int Array[], int size);
+    bool Checking_Adaptivity(int Array[], int size);
+};
+
+void Bubble_class::bubbleSortProcessShow(int *arr, int size)
+{
+
     int temp;
     for (int i = 0; i < size - 1; i++) // number of passes in the array.
     {
-        cout<<"Working on pass number : "<<i+1<<endl;
-        for (int j = 0; j < size-1-i; j++) // for positioning the elements in the array.
-                                           // size n-1-i is come for the replacing the elements.
+        cout << "Working on pass number : " << i + 1 << endl;
+        for (int j = 0; j < size - 1 - i; j++) // for positioning the elements in the array.
+                                               // size n-1-i is come for the replacing the elements.
         {
             if (arr[j] > arr[j + 1])
             {
@@ -18,103 +27,126 @@ void bubbleSortProcessShow(int arr[], int size)
             }
             for (int k = 0; k < size; k++)
             {
-                cout<<arr[k];
-                if(k==size-1){
-                    cout<<" ";
-                }else{
-                    cout<<",";
+                cout << arr[k];
+                if (k == size - 1)
+                {
+                    cout << " ";
                 }
-
+                else
+                {
+                    cout << ",";
+                }
             }
 
-            cout<<endl;
-
+            cout << endl;
         }
     }
 }
 
-void bubbleSort(int Array[],int size){
+void Bubble_class::bubbleSort(int *Array, int size)
+{
     int temp;
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < size; i++) // number of passes.
     {
-        for (int j = 0; j < size-1-i; j++)
+        for (int j = 0; j < size - 1 - i; j++)
         {
-            if(Array[j]>Array[j+1]){
-                temp=Array[j];
-                Array[j]=Array[j+1];
-                Array[j+1]=temp;
+            if (Array[j] > Array[j + 1])
+            {
+                temp = Array[j];
+                Array[j] = Array[j + 1];
+                Array[j + 1] = temp;
             }
         }
-
     }
 
     for (int i = 0; i < size; i++)
     {
-        cout<<Array[i];
-        if (i==size-1)
+        cout << Array[i];
+        if (i == size - 1)
         {
-            cout<<" ";
-        }else{
-            cout<<",";
+            cout << " ";
         }
-
+        else
+        {
+            cout << ",";
+        }
     }
+    cout << endl;
 }
 
-bool Checking_Adaptivity(int Array[],int size){
-    bool isSorted=false;
+bool Bubble_class::Checking_Adaptivity(int *Array, int size)
+{ // if whole array is sorted and check is one in the one pass only then the array is know to be adaptive array.
+    bool isSorted = false;
     for (int i = 0; i < 1; i++)
     {
-        for (int j = 0; j < size-i-1; j++)
+        for (int j = 0; j < size - i - 1; j++)
         {
-            if(Array[j]>Array[j+1]){  // checking for the adaptivity.
-                isSorted=true;
+            if (Array[j] > Array[j + 1])
+            { // checking for the adaptivity.
+                isSorted = true;
                 break;
             }
         }
-
     }
 
     return isSorted;
-
 }
 int main()
 {
-    int Array[] = {10, 0, 9, 4, 2};
-    int A2[]={1,2,3,4,5};
-    /*
-    pass 1
-    {10,0,9,4,2}
-    {10,9,0,4,2}
-    {10,9,4,0,2}
-    {10,9,4,2,0}
-    */
-    /*
-    pass 2
-    {10,9,4,2,0} // setting in the decending order .
-    */
-
-    int size = 5;
-    for (int  i = 0; i < size; i++)
+    Bubble_class obj;
+    int size;
+    int choice;
+    cout << "Enter the Size of the Array:- ";
+    cin >> size;
+    int arr[size];
+    for (int i = 0; i < size; i++)
     {
-        cout<<Array[i]; // printing the array before sorting.
-        if(i==size-1){
-            cout<<" ";
-        }else{
-            cout<<",";
+        cout << "Enter the Element " << i + 1 << " :- ";
+        cin >> arr[i];
+    }
+    cout << endl;
+    cout << endl;
+    do
+    {
+        cout << "1. Do the Bubble Sort" << endl;
+        cout << "2. Show the Process of the Bubble Sort" << endl;
+        cout << "3. Check the Adaptivity of the array" << endl;
+        cout << "4. Exit" << endl;
+        cout << endl;
+        cout << "Enter your Choice:- ";
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+        {
+            obj.bubbleSort(arr, size);
+            break;
         }
-    }
-    cout<<endl;
-    cout<<"Showing the process of the Bubble Sort: "<<endl;
-    bubbleSortProcessShow(Array, size);
-    cout<<endl;
-    if(Checking_Adaptivity(A2,size)){
-        cout<<"The array is not Sorted in one pass"<<endl;
-    }else{
-        cout<<"The Array is Sorted in one pass"<<endl;
-    }
-    cout<<"Final Sorted Array is: "<<endl;
-    bubbleSort(Array,size);
+        case 2:
+        {
+            obj.bubbleSortProcessShow(arr, size);
+            break;
+        }
+        case 3:
+        {
+            bool result;
+            result = obj.Checking_Adaptivity(arr, size);
+            if (result)
+            {
+                cout << "The given array is not checked in the one pass and hence non adapatable" << endl;
+            }
+            else
+            {
+                cout << "The given array is sorted in one pass and hence adaptive " << endl;
+            }
+            break;
+        }
+        case 4:
+            break;
+        default:
+            cout << "Wrong choice !!!!!!!!!!!!!!";
+            break;
+        }
 
-    return 0;
+    } while (choice != 4);
 }
