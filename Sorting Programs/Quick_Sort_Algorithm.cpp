2f21b3a103f372b611a4f797c88acfd7c9af3fc7
Manan Aggarwal <mananaggarwal2001@gmail.com>
2022-05-30 12:55:36 +0530

    Quick Sort Modified

--- Sorting Programs/Quick_Sort_Algorithm.cpp
+++ Sorting Programs/Quick_Sort_Algorithm.cpp
@@ -14,49 +14,48 @@ void PrintArray(int array[], int size)
             cout << ",";
         }
     }
-    cout<<endl;
+    cout << endl;
 }
 
 int partition(int Array[], int low, int high)
 {
-    int pivot = Array[low];
-    int i = low + 1;
-    int j = high;
-    int temp;
+    int pivot = Array[low]; // taking the first element as the pivot element.
+    int i = low + 1;        // Taking the element after the pivot element.
+    int j = high;           // Taking the element which is at the end of  the array.
+    int temp;               // used for the doing the swapping of the variables.
     do
     {
         while (Array[i] <= pivot) // we have to find  the element larger than pivot element .
         {
             i++;
         }
-        while (Array[j] > pivot) // we have to find the element smaller than pivot element.
+        while (Array[j] > pivot) // we have to find the element smaller than pivot element  .
         {
             j--;
         }
-        if (i < j)
+        if (i < j) // swapping of the element is done in when the i is less than j.
         {
             temp = Array[i];
             Array[i] = Array[j];
             Array[j] = temp;
         }
-        /* code */
-    } while (i < j);
 
+    } while (i < j);
+    // Swap A[low] and A[j] for positioning the pivot element at the right place when sorting the array.
     temp = Array[low];
     Array[low] = Array[j];
     Array[j] = temp;
     return j;
 }
 
-void Quick_Sort(int Array[], int low, int high,int size)
+void Quick_Sort(int Array[], int low, int high, int size)
 {
     int partitionIndex; // Index of the pivot after partition.
     if (low < high)
     {
         partitionIndex = partition(Array, low, high);
-        PrintArray(Array,size);
-        Quick_Sort(Array, low, partitionIndex - 1,size);  // sort left sub array.
-        Quick_Sort(Array, partitionIndex + 1, high,size); // sort right sub array.
+        Quick_Sort(Array, low, partitionIndex - 1, size);  // sort left sub array.
+        Quick_Sort(Array, partitionIndex + 1, high, size); // sort right sub array.
     }
 }
 int main()
@@ -73,7 +72,7 @@ int main()
 
     cout << "Unsorted Array:-" << endl;
     PrintArray(array, size);
-    Quick_Sort(array,0,size-1,size);
+    Quick_Sort(array, 0, size - 1, size);
     cout << "Sorted Array is:-" << endl;
     PrintArray(array, size);
 
